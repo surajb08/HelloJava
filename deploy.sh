c83da82b3c44046e3d0cc8f286e3385ee29daa9b
Suraj <suraj@localhost.localdomain>
2020-06-30 04:46:32 +0530

    change in pom.xml and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,22 @@
 
 
 gcr_url="gcr.io/gcptraining-17042017/hello-java"
-version=${1:-"0.0.1"}
+version=${1:-"latest"}
 
+# Creates source-context.json file in target directory
+gcloud debug source gen-repo-info-file --output-directory="target/classes/"
+
+# Build maven package as specified in pom.xml
+./mvnw -DskipTests package
+
+# Complete image with version
 image=$gcr_url:$version
 
+# Docker build image with above tag
 docker build -t $image .
 
+# Push the built docker image to GCR
 gcloud docker -- push $image
 
+# Changes the image of the current deployment with new image
 kubectl set image deployment/hello-world-py hello-world-py=$image -n demo --record
